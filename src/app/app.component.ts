b44b822f6222a600c32d6551f727fadc9628e2cb
Mauricio Rodriguez Porcel <x-tremecomputer@hotmail.com>
2018-03-22 00:15:38 +0100

    centrado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import { SelectControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
-  title = 'app';
+  title = 'COMANDA RESTÓ-BAR';
+
+
+  platos: string[] = [
+    'Costillas de cerdo al verdeo',
+    'Ñoquis al tuco',
+    'Pizza Mediterranea',
+  ]
+    
+  public plato=['Esto es una linea prueba'];
+
+/* blabla= function (){
+this.platos.selectedIndex
+ }*/
+  
+ 
+
+  public addLinea()
+  {
+
+    //this.plato.push( + '  Orden '+(this.plato.length+1))
+
+    //this.plato.push(this.platos[""] + '  Orden '+(this.plato.length+1))
+    
+    
+    //his.platos.indexOf(
+  }
+  
+  public delLinea(i)
+  {
+	  this.plato.splice(i,1);
+  }
+
+ 
+  
 }
